2e401b3926810a041bbadc7d9b669daef74718d0
Bastien <bastienprog@gmail.com>
2020-05-22 22:52:51 +0200

    (added DerivationEngine to git repo)

---
+++ src/DerivationEngine.java
@@ -0,0 +1,88 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DerivationEngine {
+	private ArrayList<Rule> _ruleSet;
+
+	private HashMap<String,ArrayList<String>> _derivations;
+
+	public DerivationEngine() {
+		_ruleSet = new ArrayList<>();
+		_derivations = new HashMap<>();
+	}
+
+	public int nbRules() {
+		return _ruleSet.size();
+	}
+
+	public void removeRule(int index) {
+		_ruleSet.remove(index);
+	}
+
+	public void clearRules() {
+		_ruleSet.clear();
+	}
+
+	public void clearDerivations() {
+		_derivations.clear();
+	}
+
+	public ArrayList<String> getDerivations(String mot){
+		return _derivations.get(mot);
+	}
+
+	public void loadRules(String path) {
+		File ruleFile = new File(path);
+		if(ruleFile.exists()) {
+			try {
+				Scanner reader = new Scanner(ruleFile);
+				while (reader.hasNextLine()) {
+					_ruleSet.add(new Rule(reader.nextLine()));
+				}
+				reader.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+	
+	public void run(String word) {
+		if(!_ruleSet.isEmpty()) {
+			for(Rule rule : _ruleSet) {
+				String applicationResult = rule.apply(word);
+				if(!applicationResult.isBlank()) {
+					if(!_derivations.containsKey(word)) {
+						_derivations.put(word, new ArrayList<>());
+					}
+					
+					_derivations.get(word).add(applicationResult);
+				}
+			}
+		}
+	}
+	
+	public void clear() {
+		clearRules();
+		clearDerivations();
+	}
+	
+	public String toString() {
+		String display = "";
+		
+		display += "----Règles----\n";
+		for(Rule r : _ruleSet) {
+			display += r.toString() + "\n";
+		}
+		
+		display += "\n----Dérivations----\n";
+		for(String k : _derivations.keySet()) {
+			display += k + " => " + _derivations.get(k) + "\n";
+		}
+		
+		return display;
+	}
+}
